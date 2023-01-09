f316644375143b5f3e90527f2c65b1b391944e71
Bobby Luly <bobbyluly@gmail.com>
2023-01-08 17:31:13 -0700

    Test- create test for #item_names

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -19,12 +19,20 @@ RSpec.describe Auction do
     end
   end
 
-  describe '#add_items' do
+  describe '#add_items and return #item_names' do
     it 'can add items to empty items array' do
       auction.add_item(item1)
       auction.add_item(item2)
 
       expect(auction.items).to eq([item1, item2])
     end
+
+    it 'can return item names in items array' do
+      auction.add_item(item1)
+      auction.add_item(item2)
+
+      expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+    end
+
   end
 end
