f31aa3e95aab724251f713ab42751d06f2da4c62
Bobby Luly <bobbyluly@gmail.com>
2023-01-08 18:13:28 -0700

    Test- crete test for #unpopular_items

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -76,7 +76,15 @@ RSpec.describe Auction do
 
         expect(item1.current_high_bid).to eq(22)
       end
-      
+
+      it 'returns an array of #unpopular_items' do
+        item1.add_bid(attendee2, 20)
+        item1.add_bid(attendee1, 22)
+        item4.add_bid(attendee3, 50)
+
+        expect(auction.unpopular_items).to eq([item2, item3, item5])
+      end
+
     end
   end
 
