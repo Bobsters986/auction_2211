218c6f7ea2532836601ec259c87ed5c62f020431
Bobby Luly <bobbyluly@gmail.com>
2023-01-08 17:27:09 -0700

    Test- create #add_item test

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -4,6 +4,10 @@ require './lib/attendee'
 
 RSpec.describe Auction do
   let(:auction) { Auction.new }
+  let(:attendee) { Attendee.new(name: "Megan", budget: "$50") }
+  let(:item1) { Item.new('Chalkware Piggy Bank') }
+  let(:item2) { Item.new('Bamboo Picture Frame') }
+
 
   describe '#initialize' do
     it 'exists' do
@@ -12,6 +16,15 @@ RSpec.describe Auction do
 
     it 'has attributes' do
       expect(auction.items).to eq([])
+    end
+  end
+
+  describe '#add_items' do
+    it 'can add items to empty items array' do
+      auction.add_item(item1)
+      auction.add_item(item2)
+
+      expect(auction.items).to eq([item1, item2])
     end
   end
 end
