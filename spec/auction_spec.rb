163e7ee796460290a836aeea6f64df5c3b514ef8
Bobby Luly <bobbyluly@gmail.com>
2023-01-08 17:21:01 -0700

    Create auction initialize tests

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -2,3 +2,15 @@ require './lib/item'
 require './lib/auction'
 require './lib/attendee'
 
+RSpec.describe Auction do
+  let(:auction) { Auction.new }
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(item1).to be_an(Auction)
+    end
+
+    it 'has attributes' do
+    end
+  end
+end
